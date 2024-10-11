706ff17f004de5f7180b5e0f7bebfa4e03cd4131
Dante Alves <dantealves8@gmail.com>
2024-10-11 16:29:21 -0300

    Finalizado

--- ex01.cpp
+++ ex01.cpp
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
+
+/* 1) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva 
+um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a 
+sequência.
+*/
 
 //Variável global
 int verificar;
 
 //Procedimento para printar a sequência de Fibonacci e verificar se o número escolhido pertence à sequência
 void fibonacci(int N){
-	
+	//Variáveis
 	int num1 = 0, num2 = 1, aux;
 	
 	printf("\nSequência de Fibonacci: ");
@@ -25,9 +31,9 @@ void fibonacci(int N){
 	printf("%d, %d...\n", num1, num2); //Para printar dois valores da sequência acima de N
 }
 
+
 int main() {
-	setlocale(0, "Portuguese"); //Deixando em pt-br
-	
+	setlocale(LC_ALL, "Portuguese"); //Deixando em pt-br
 	//Variáveis
 	int N;
 	float N_float;
